44c297470c9a6a1e1fa6ee3f2afe8e5a878595dc
Guang-Lin Wei <guanglinwei@gmail.com>
2025-04-13 10:29:11 -0400

    fix signout problem

--- src/pages/Starting.tsx
+++ src/pages/Starting.tsx
@@ -12,11 +12,12 @@ function Starting() {
 
   // If user is logged in and auth state has finished loading, redirect
   useEffect(() => {
+    // if (!user) setPageVisible(true);
     if (!loading && user) {
         setPageVisible(false);
         setTimeout(() => {
             navigate("/home");
-
+            setPageVisible(true);
         }, 500)
     }
   }, [user, loading, navigate]);
